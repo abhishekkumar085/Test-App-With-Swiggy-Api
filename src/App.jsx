b853f733dec2ac9d229472f5ecda637dadb0dcef
abhishekkumar085 <abhishekkumar08528@gmail.com>
2024-04-07 01:45:48 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import RestaurantsCard from './components/RestaurantsCard';
+import LoadingSkeleton from './components/LoadingSkeleton';
+import restaurantsData from './constant';
+import {  toast } from 'react-toastify';
+
+
+function App() {
+  const [restaurants, setRestaurants] = useState(restaurantsData);
+  const [isLoading, setIsLoading] = useState(true); // State to track fetch loading
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING'
+      );
+      const jsonData = await response.json();
+      toast.success("Response Success 👍👍👍")
+      const fetchedRestaurants =
+        jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants;
+      setRestaurants(fetchedRestaurants);
+      setIsLoading(false); // Update loading state to false after data is fetched
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      toast.error(error,"🤡🤡🤡🤡")
+      setIsLoading(false); // Ensure loading state is set to false even in case of error
+    }
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4 mt-2">
+      {isLoading ? ( 
+        Array.from({ length: 10 }).map((_, index) => <LoadingSkeleton key={index} />)
+      ) : (
+        
+        restaurants.map((restaurant,i) => (
+          <RestaurantsCard {...restaurant.info} key={restaurant.id || i} />
+        ))
+      )}
+    </div>
+  );
+}
 
 // import React, { useEffect, useState } from 'react';
 // import RestaurantsCard from './components/RestaurantsCard';
@@ -38,53 +85,6 @@
 // }
 
 // export default App;
-import React, { useEffect, useState } from 'react';
-import RestaurantsCard from './components/RestaurantsCard';
-import LoadingSkeleton from './components/LoadingSkeleton';
-import restaurantsData from './constant';
-import {  toast } from 'react-toastify';
-
-
-function App() {
-  const [restaurants, setRestaurants] = useState(restaurantsData);
-  const [isLoading, setIsLoading] = useState(true); // State to track fetch loading
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  async function fetchData() {
-    try {
-      const response = await fetch(
-        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING'
-      );
-      const jsonData = await response.json();
-      // toast.success("Response Success 👍👍👍")
-      const fetchedRestaurants =
-        jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants;
-      setRestaurants(fetchedRestaurants);
-      setIsLoading(false); // Update loading state to false after data is fetched
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      toast.error(error,"🤡🤡🤡🤡")
-      setIsLoading(false); // Ensure loading state is set to false even in case of error
-    }
-  }
-
-  return (
-    <div className="flex flex-wrap justify-center gap-4 mt-2">
-      {isLoading ? ( 
-        Array.from({ length: 10 }).map((_, index) => <LoadingSkeleton key={index} />)
-      ) : (
-        
-        restaurants.map((restaurant,i) => (
-          <RestaurantsCard {...restaurant.info} key={restaurant.id || i} />
-        ))
-      )}
-    </div>
-  );
-}
 
 export default App;
 
